779b700fc8474444e39736e8d0bc628161c81dc2
Theo-flux <72856939+Theo-flux@users.noreply.github.com>
2025-02-03 21:35:05 +0100

    test result uploads.

--- src/app/(modules)/lab-tech/tests/components/ResultUploadModal.tsx
+++ src/app/(modules)/lab-tech/tests/components/ResultUploadModal.tsx
@@ -1,8 +1,18 @@
+import FieldSet from '@/atoms/fields/FieldSet';
+import Input from '@/atoms/fields/Input';
 import { XModal } from '@/atoms/modal';
+import { Paragraph, SubTitle } from '@/atoms/typographys';
 import { useStore } from '@/store';
 import { AppModals } from '@/store/AppConfigStore/appModalTypes';
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table';
 
 const ResultUploadModal = () => {
   const {
@@ -13,8 +23,60 @@ const ResultUploadModal = () => {
       closeModal={() => toggleModals({ name: AppModals.RESULT_UPLOAD_MODAL, open: false })}
       bgClose={false}
       isOpen={isOpen.RESULT_UPLOAD_MODAL}
+      className="!max-w-[1440px]"
+      title="Result Upload"
     >
-      <div>result upload</div>
+      <div className="flex w-full flex-col space-y-8">
+        <div className="flex w-full flex-col space-y-1">
+          <Paragraph className="text-lg !font-medium" text="Test Information" />
+
+          <div className="flex w-full flex-col space-y-1">
+            <div className="flex w-full flex-col items-center justify-between gap-2 md:flex-row">
+              <FieldSet className="md:w-[60%]" legend="Patient Name" text="Ahmed Musa Zola" />
+              <FieldSet className="md:w-[20%]" legend="Test type" text="CBC" />
+              <FieldSet className="md:w-[20%]" legend="Test Date" text="2nd Sept. 2024" />
+            </div>
+
+            <div className="flex w-full flex-col items-center justify-between gap-2 md:flex-row">
+              <FieldSet className="md:w-[80%]" legend="Clinician Name" text="Lawrence Afolabi" />
+              <FieldSet className="md:w-[20%]" legend="Signature" text="Lawrence" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex w-full flex-col space-y-1">
+          <Paragraph className="text-lg !font-medium" text="Test Result" />
+
+          <div className="flex w-full flex-col space-y-1">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Parameter</TableHead>
+                  <TableHead>Result</TableHead>
+                  <TableHead>Range</TableHead>
+                  <TableHead className="w-[30px]">Unit</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <Input />
+                  </TableCell>
+                  <TableCell>
+                    <Input />
+                  </TableCell>
+                  <TableCell>
+                    <Input />
+                  </TableCell>
+                  <TableCell className="w-[30px]">
+                    <Input className="w-full" />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </div>
     </XModal>
   );
 };
--- src/atoms/modal/index.tsx
+++ src/atoms/modal/index.tsx
@@ -2,6 +2,17 @@
 import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react';
 import { Fragment } from 'react';
 import { GoX } from 'react-icons/go';
+import { SubTitle } from '../typographys';
+
+interface IXModalProps {
+  className?: string;
+  children: React.ReactNode;
+  isOpen: boolean;
+  closeModal: () => void;
+  bgClose?: boolean;
+  showClose?: boolean;
+  title?: string;
+}
 
 export const XModal = ({
   className,
@@ -9,15 +20,9 @@ export const XModal = ({
   isOpen,
   closeModal,
   showClose = true,
-  bgClose = true
-}: {
-  className?: string;
-  children: React.ReactNode;
-  isOpen: boolean;
-  closeModal: () => void;
-  bgClose?: boolean;
-  showClose?: boolean;
-}) => {
+  bgClose = true,
+  title
+}: IXModalProps) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative" onClose={() => (bgClose ? closeModal() : null)}>
@@ -34,7 +39,7 @@ export const XModal = ({
         </TransitionChild>
 
         <div className="fixed inset-0 z-[999] overflow-y-auto bg-black/80 backdrop-blur-[5px]">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="flex min-h-full items-start justify-center p-4 text-center">
             <TransitionChild
               as={Fragment}
               enter="ease-out duration-300"
@@ -45,10 +50,11 @@ export const XModal = ({
               leaveTo="opacity-0 scale-95"
             >
               <DialogPanel
-                className={`flex w-full transform flex-col items-center justify-center space-y-1 rounded-2xl bg-white px-4 py-6 text-left align-middle shadow-xl transition-all md:w-[550px] ${className}`}
+                className={`flex w-full max-w-[550px] transform flex-col items-center justify-center space-y-2 rounded-xl border bg-white p-4 text-left align-middle shadow-xl transition-all ${className}`}
               >
                 {showClose && (
-                  <div className="flex w-full items-center justify-end">
+                  <div className="flex w-full items-center justify-between">
+                    <SubTitle text={title ?? ''} />
                     <div
                       onClick={closeModal}
                       className="flex h-[32px] w-[32px] cursor-pointer items-center justify-center rounded-full bg-bgGray hover:bg-[#00000010]"
