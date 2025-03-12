85db5fc18476a0d529a3c8ab53f2d2054eae1343
Techtees <taiwojoshuaoye@gmail.com>
2025-03-12 08:52:18 +0100

    transaction styling table

--- src/app/(modules)/patient/billing/history/component/TransactionHistoryTable.tsx
+++ src/app/(modules)/patient/billing/history/component/TransactionHistoryTable.tsx
@@ -18,8 +18,22 @@ import {
   DropdownMenuGroup
 } from '@/components/ui/dropdown-menu';
 import { DownloadCloudIcon } from 'lucide-react';
+import { dateTimeUTC } from '@/utils/date';
 
 const TransactionHistoryTable = ({ data }: BillingHistory) => {
+  const getStatusColor = (status: string) => {
+    switch (status) {
+      case 'PENDING':
+        return 'bg-[#6226EF]/20 text-blue-400';
+      case 'COMPLETED':
+        return 'bg-green-200/30 text-green-500';
+      case 'FAILED':
+        return 'bg-red-200/30 text-red-500';
+      default:
+        return 'bg-gray-200/30 text-gray-500';
+    }
+  };
+
   return (
     <div className="w-full overflow-clip rounded-lg bg-white">
       <Table>
@@ -37,10 +51,16 @@ const TransactionHistoryTable = ({ data }: BillingHistory) => {
           {data?.payments.map((test) => (
             <TableRow key={test.invoiceNo}>
               <TableCell>{test.invoiceNo}</TableCell>
-              <TableCell>{test.paymentDate}</TableCell>
+              <TableCell>{dateTimeUTC(test.paymentDate)}</TableCell>
               <TableCell>{test.paymentMethod}</TableCell>
               <TableCell>{test.amountPaid}</TableCell>
-              <TableCell>{test.paymentStatus}</TableCell>
+              <TableCell>
+                <span
+                  className={`${getStatusColor(test.paymentStatus)} rounded-lg px-5 py-2 font-[500]`}
+                >
+                  {test.paymentStatus}
+                </span>
+              </TableCell>
               {/* download  */}
               <TableCell>
                 <DownloadCloudIcon className="cursor-pointer" />
