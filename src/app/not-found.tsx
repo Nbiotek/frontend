d23cc00e6a598fc4fbbce5e7373323c23d3c8d26
Theo-flux <tifluse@gmail.com>
2025-04-17 11:41:15 +0100

    fix unauthorised and not found build error.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import ROUTES from '@/constants/routes';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href={ROUTES.HOME.path}>Return Home</Link>
+    </div>
+  );
+}
