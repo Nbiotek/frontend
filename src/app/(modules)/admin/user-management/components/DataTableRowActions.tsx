ce47039b40f750f59797c6cc8e72a6881bf08936
Theo-flux <tifluse@gmail.com>
2025-05-27 09:57:00 +0100

    user table datatable components

---
+++ src/app/(modules)/admin/user-management/components/DataTableRowActions.tsx
@@ -0,0 +1,46 @@
+import { Row } from '@tanstack/react-table';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { User } from './data/schema';
+import { Edit, EllipsisVerticalIcon, TrashIcon } from 'lucide-react';
+
+interface DataTableRowActionsProps {
+  row: Row<User>;
+}
+
+export function DataTableRowActions({ row }: DataTableRowActionsProps) {
+  return (
+    <>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+            <EllipsisVerticalIcon className="h-4 w-4" />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[160px]">
+          <DropdownMenuItem onClick={() => {}}>
+            Edit
+            <DropdownMenuShortcut>
+              <Edit size={16} />
+            </DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => {}} className="text-red-500!">
+            Delete
+            <DropdownMenuShortcut>
+              <TrashIcon size={16} />
+            </DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
