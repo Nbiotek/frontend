ed2d1bd315b1b0189d36db230329092179c4b479
Techtees <taiwojoshuaoye@gmail.com>
2025-09-24 18:00:13 +0100

    fix/ availability on doctor

--- src/app/(modules)/doctor/(dashboard)/components/RecentActivityTable.tsx
+++ src/app/(modules)/doctor/(dashboard)/components/RecentActivityTable.tsx
@@ -11,22 +11,83 @@ import {
   TableRow
 } from '@/components/ui/table';
 import { dateTimeUTC } from '@/utils/date';
-import { EllipsisVertical } from 'lucide-react';
+import { CheckCircle, EllipsisVertical, MapPin } from 'lucide-react';
 
 import { useRecentActivity } from '@/hooks/doctor/useDashboard';
 import { Text } from '@/lib/utils/Text';
+import { useState } from 'react';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { doctorDashboardService } from '@/requests/doctor';
+import toast from 'react-hot-toast';
+import { AUTH } from '@/constants/api';
+import { Switch } from '@/components/ui/switch';
+import { useFetchProfile } from '@/hooks/user/useFetchProfile';
 
 const RecentActivityTable = () => {
   const { isLoading, data: recentActivity } = useRecentActivity();
+  const [isCheckedIn, setIsCheckedIn] = useState(false);
 
   const reviews = recentActivity?.data?.reviews || [];
   const hasNoData = reviews.length === 0;
+
+  const { data: userProfile } = useFetchProfile();
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: doctorDashboardService.updateAvailability,
+
+    onError: () => {
+      toast.error('Unable to update availability now.');
+    },
+    onMutate: () => {},
+    onSuccess: (data) => {
+      const status = data.status;
+
+      if (status === 'Available') {
+        toast.success('You are checked in!');
+      } else {
+        toast.success('You are checked out!');
+      }
+
+      queryClient.invalidateQueries({ queryKey: [AUTH.GET_PROFILE] });
+    }
+  });
   return (
     <>
       <div className="w-full rounded-lg px-4 py-3">
-        <Text className="border-b-1.5 mb-4" variant="title" weight="bold">
-          Recent Activity
-        </Text>
+        <div className="flex items-center justify-between p-3">
+          <Text className="border-b-1.5 " variant="title" weight="bold">
+            Recent Activity
+          </Text>
+
+          <div
+            className={`flex items-center space-x-3 rounded-lg border-2 px-4 py-2 transition-all duration-200 ${
+              isCheckedIn
+                ? 'bg-green-50 text-green-800 border-green-200'
+                : 'bg-orange-50 border-orange-200 text-orange-800'
+            }`}
+          >
+            <div className="flex items-center space-x-2">
+              {isCheckedIn ? (
+                <CheckCircle size={18} className="text-green-600" />
+              ) : (
+                <MapPin size={18} className="text-orange-600" />
+              )}
+              <span className="font-medium">{isCheckedIn ? 'Checked In' : 'Check In'}</span>
+            </div>
+            <Switch
+              checked={userProfile?.status === 'Available'}
+              onCheckedChange={setIsCheckedIn}
+              disabled={isPending}
+              onClick={() =>
+                mutate({
+                  status: userProfile?.status === 'Available' ? 'Unavailable' : 'Available'
+                })
+              }
+            />
+          </div>
+        </div>
         <Table>
           <TableHeader className="bg-neutral-50 text-black">
             <TableRow className="text-black">
