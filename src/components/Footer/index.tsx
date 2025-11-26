a2412b7171e7e90231ee6a4d609c57cd2971048e
Techtees <taiwojoshuaoye@gmail.com>
2025-11-26 19:20:02 +0100

    fix ui content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Facebook, Twitter, Instagram } from 'lucide-react';
 import { FaFacebook } from 'react-icons/fa6';
-import { PiTwitterLogo } from 'react-icons/pi';
+import { PiTwitterLogo, PiWhatsappLogo } from 'react-icons/pi';
 import { FiInstagram } from 'react-icons/fi';
 import ROUTES from '@/constants/routes';
 
@@ -206,22 +206,9 @@ const Footer = () => {
                 href="#"
                 className="bg-gray-800 flex h-10 w-10 items-center justify-center rounded-full text-neutral-200 transition hover:bg-blue-400 hover:text-white"
               >
-                <PiTwitterLogo size={20} className="" />
+                <PiWhatsappLogo size={20} className="" />
               </a>
-              <a
-                href="#"
-                className="bg-gray-800 flex h-10 w-10 items-center justify-center rounded-full text-neutral-200 transition hover:bg-blue-400 hover:text-white"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.441 16.892c-2.102.144-6.784.144-8.883 0-2.276-.156-2.541-1.27-2.558-4.892.017-3.629.285-4.736 2.558-4.892 2.099-.144 6.782-.144 8.883 0 2.277.156 2.541 1.27 2.559 4.892-.018 3.629-.285 4.736-2.559 4.892zm-6.441-7.234l4.917 2.338-4.917 2.346v-4.684z" />
-                </svg>
-              </a>
+
               <a
                 href="#"
                 className="bg-gray-800 flex h-10 w-10 items-center justify-center rounded-full text-neutral-200 transition hover:bg-blue-400 hover:text-white"
